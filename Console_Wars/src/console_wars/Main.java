77ea3b9280128ec79ee5869dbd579b7791cd9a25
11samype <11samype@gmail.com>
2014-02-06 23:40:39 -0500

    added some useful queries

--- Console_Wars/src/console_wars/Main.java
+++ Console_Wars/src/console_wars/Main.java
@@ -22,6 +22,11 @@ public class Main {
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.start();
+		SQLBackend.connectToDB();
+		SQLBackend.getCompanies();
+		SQLBackend.getUnits();
+		SQLBackend.getGenerals();
+		SQLBackend.getRegions();
 		
 	}
 }
--- Console_Wars/src/console_wars/SQLBackend.java
+++ Console_Wars/src/console_wars/SQLBackend.java
@@ -5,48 +5,217 @@ import java.math.*;
 
 /**
  * TODO Put here a description of what this class does.
- *
- * @author samynpd.
- *         Created Feb 5, 2014.
+ * 
+ * @author samynpd. Created Feb 5, 2014.
  */
 public class SQLBackend {
 
+	private static String username = "";
+	private static String password = "";
+	private static String url = "jdbc:sqlserver://titan.cs.rose-hulman.edu;databaseName=Console_Wars";
+
+	private static Connection conn;
+
 	/**
 	 * TODO Put here a description of what this method does.
-	 *
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		try{
-		
-		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		
-		String username = "";
-		String password = "";
-		String url = "jdbc:sqlserver://titan.cs.rose-hulman.edu;databaseName=Console_Wars";
-		
-		Connection conn = DriverManager.getConnection(url, username, password);
-		if(!(conn == null)) {
-			System.out.println("connected");
-		}
-		
-		Statement s1 = conn.createStatement();
-		ResultSet rs = s1.executeQuery("SELECT TOP 1000 * FROM [Console_Wars].[dbo].[Units]");
-		String[] result = new String[20];
-		
-		if(rs!=null) {
-			while(rs.next()) {
+		try {
 
-				
-				String mem = rs.getString(1);
-				System.out.println("Result is " + mem);
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+			connectToDB();
+
+			Statement s1 = conn.createStatement();
+			ResultSet rs = s1
+					.executeQuery("SELECT TOP 1000 [name],[HQ],[perks] FROM [Console_Wars].[dbo].[Companies]");
+			// String[] result = new String[20];
+
+			if (rs != null) {
+				while (rs.next()) {
+
+					String name = rs.getString("name");
+					String hq = rs.getString("HQ");
+					int perks = rs.getInt("perks");
+					System.out.println(name + " " + hq + " " + " " + perks);
+				}
 			}
-		}
-		
+
 		} catch (Exception e) {
 			e.printStackTrace();
-		}	
+		}
 	}
 
+	/**
+	 * Connect to DB
+	 * 
+	 */
+	public static void connectToDB() {
+		try {
+			conn = DriverManager.getConnection(url, username, password);
+			if (!(conn == null)) {
+				System.out.println("connected");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void getCompanies() {
+
+		try {
+
+			Statement s1 = conn.createStatement();
+			ResultSet rs = s1
+					.executeQuery("SELECT TOP 1000 [name],[HQ],[perks] FROM [Console_Wars].[dbo].[Companies]");
+
+			if (rs != null) {
+				while (rs.next()) {
+
+					String name = rs.getString("name");
+					String hq = rs.getString("HQ");
+					int perks = rs.getInt("perks");
+					System.out.println(name + " " + hq + " " + " " + perks);
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void getUnits() {
+
+		try {
+
+			Statement s1 = conn.createStatement();
+			ResultSet rs = s1
+					.executeQuery("SELECT TOP 1000 [unitName],[ability],[attack],[defense],[attackRange],[type],[name],[genName],[mobility],[life] FROM [Console_Wars].[dbo].[Units]");
+
+			if (rs != null) {
+				while (rs.next()) {
+
+					String unitName = rs.getString("unitName");
+					int ability = rs.getInt("ability");
+					int attack = rs.getInt("attack");
+					int defense = rs.getInt("defense");
+					int attackRange = rs.getInt("attackRange");
+					int type = rs.getInt("type");
+					String name = rs.getString("name");
+					String genName = rs.getString("genName");
+					int mobility = rs.getInt("mobility");
+					int life = rs.getInt("life");
+
+					System.out.println(unitName + " " + ability + " " + attack
+							+ " " + defense + " " + attackRange + " " + type
+							+ " " + name + " " + genName + " " + mobility + " "
+							+ life);
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
+	public static void getGenerals() {
+
+		try {
+
+			Statement s1 = conn.createStatement();
+			ResultSet rs = s1
+					.executeQuery("SELECT TOP 1000 [unitName],[ability],[attack],[defense],[attackRange],[type],[name],[genName],[mobility],[life] FROM [Console_Wars].[dbo].[Units] WHERE unitName = genName");
+
+			if (rs != null) {
+				while (rs.next()) {
+
+					String unitName = rs.getString("unitName");
+					int ability = rs.getInt("ability");
+					int attack = rs.getInt("attack");
+					int defense = rs.getInt("defense");
+					int attackRange = rs.getInt("attackRange");
+					int type = rs.getInt("type");
+					String name = rs.getString("name");
+					String genName = rs.getString("genName");
+					int mobility = rs.getInt("mobility");
+					int life = rs.getInt("life");
+
+					System.out.println(unitName + " " + ability + " " + attack
+							+ " " + defense + " " + attackRange + " " + type
+							+ " " + name + " " + genName + " " + mobility + " "
+							+ life);
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void getRegions() {
+
+		try {
+
+			Statement s1 = conn.createStatement();
+			ResultSet rs = s1
+					.executeQuery("SELECT TOP 1000 [regionID],[controlling],[controlValue],[resourcesAmmo],[resourcesFuel] FROM [Console_Wars].[dbo].[Region]");
+
+			if (rs != null) {
+				while (rs.next()) {
+
+					int regionID = rs.getInt("regionID");
+					String controlling = rs.getString("controlling");
+					int controlValue = rs.getInt("controlValue");
+					int resourcesAmmo = rs.getInt("resourcesAmmo");
+					int resourcesFuel = rs.getInt("resourcesFuel");
+
+					System.out.println(regionID + " " + controlling + " " + controlValue + " " + resourcesAmmo + " " + resourcesFuel);
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void getPlayer() {
+		//TODO
+		try {
+
+			Statement s1 = conn.createStatement();
+			ResultSet rs = s1
+					.executeQuery("SELECT TOP 1000 [regionID],[controlling],[controlValue],[resourcesAmmo],[resourcesFuel] FROM [Console_Wars].[dbo].[Region]");
+
+			if (rs != null) {
+				while (rs.next()) {
+
+					int regionID = rs.getInt("regionID");
+					String controlling = rs.getString("controlling");
+					int controlValue = rs.getInt("controlValue");
+					int resourcesAmmo = rs.getInt("resourcesAmmo");
+					int resourcesFuel = rs.getInt("resourcesFuel");
+
+					System.out.println(regionID + " " + controlling + " " + controlValue + " " + resourcesAmmo + " " + resourcesFuel);
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void queryDB(String query) {
+		// not useful since dont know return values
+
+		try {
+			Statement s1 = conn.createStatement();
+			ResultSet rs = s1.executeQuery(query);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
