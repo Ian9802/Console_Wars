cf752a316bd1d6044072d95a1f1178dd1818b545
Thomas Morris <morrista@rose-hulman.edu>
2014-02-11 14:00:22 -0500

    re-entering Player Class

---
+++ Console_Wars/src/console_wars/Player.java
@@ -0,0 +1,17 @@
+package console_wars;
+/**
+ * A Player
+ * 
+ * @author samnpd
+ *
+ */
+public class Player {
+
+	private Company faction;
+	
+	Player(Company faction) {
+		this.faction = faction;
+	}
+	
+}
+
