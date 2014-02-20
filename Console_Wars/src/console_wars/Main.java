4f04ae2b7e1520e75498642d2968f13b3bdf79d7
11samype <11samype@gmail.com>
2014-02-19 21:05:52 -0500

    comments

--- Console_Wars/src/console_wars/Main.java
+++ Console_Wars/src/console_wars/Main.java
@@ -14,10 +14,13 @@ public class Main {
 	 * Constants
 	 */
 	public static final Dimension WINDOW_SIZE = new Dimension(720, 720);
+	@SuppressWarnings("javadoc")
 	public static final Dimension FRAME_SIZE = new Dimension(Main.WINDOW_SIZE.width + 17, Main.WINDOW_SIZE.height + 39);
 	@SuppressWarnings("javadoc")
 	public static final int TILE_SIZE = WINDOW_SIZE.height / 10;
+	@SuppressWarnings("javadoc")
 	public static final int NUM_PLAYERS = 3;
+	@SuppressWarnings("javadoc")
 	public static final Dimension BOARD_DIMENSION_BY_TILE = new Dimension(10, 10);
 	
 	/**
